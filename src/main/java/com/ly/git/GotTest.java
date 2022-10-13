ae9c9ae02f934273019f3813cb2d1ccd01767d93
Lli <3048861182@qq.com>
2022-10-13 14:34:51 +0800

    second commit

--- src/main/java/com/ly/git/GotTest.java
+++ src/main/java/com/ly/git/GotTest.java
@@ -8,5 +8,6 @@ package com.ly.git;
 public class GotTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git! hello world!");
     }
 }

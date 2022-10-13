ffbb5bc3d7deae136268058210b6814a986a8c75
Lli <3048861182@qq.com>
2022-10-13 14:28:47 +0800

    first commit

---
+++ src/main/java/com/ly/git/GotTest.java
@@ -0,0 +1,12 @@
+package com.ly.git;
+
+/**
+ * @author ly
+ * @Date:2022/10/13/14:23
+ * @Description:
+ */
+public class GotTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
